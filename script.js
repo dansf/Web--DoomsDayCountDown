5950a55c51309fed7cd9f859f09cc1e17c742c86
Dansf <dansf.3009@gmail.com>
2022-02-21 23:16:52 -0300

    Finishing the project

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 var counter = document.querySelector(".counter");
 var followers = document.querySelector(".followers");
-var count = 0;
-var doomsDay = new Date("2030, 02, 18");
-var doomsDayCount = new Date();
-var number = doomsDayCount.setTime(doomsDay.getUTCDate());
-number = Date.UTC(2030, 2, 18, 0, 0, 0, 0);
-counter.innerText = number;
-// setInterval(() => {
-//   if (count <= 1000) {
-//     count++;
-//     counter.innerText = count;
-//   }
-// }, 1000);
-// setTimeout(() => {
-//   followers.innerText = "Followers on Instagram";
-// }, 5000);
+// Count down date
+var doomsDay = new Date("Feb 18, 2030 12:00:00").getTime();
+// Upadata the count down every 1 second
+var updateCountDown = setInterval(function () {
+    // Get Today date and time
+    var now = new Date().getTime();
+    // Find the distance between now and the count down date (doomsDay)
+    var distance = doomsDay - now;
+    // Calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Display the result in the browser
+    counter.innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+    // If the count down is finished, will be written that text below
+    if (distance < 0) {
+        clearInterval(updateCountDown);
+        counter.innerHTML = "The world already ended!";
+    }
+}, 1000);
+var audio = document.getElementById("audio");
+audio.play();
--- script.ts
+++ script.ts
@@ -1,21 +1,33 @@
 var counter = document.querySelector(".counter");
 var followers = document.querySelector(".followers");
 
-var count = 0;
-var doomsDay = new Date("2030, 02, 18");
-var doomsDayCount = new Date();
+// Count down date
+var doomsDay = new Date("Feb 18, 2030 12:00:00").getTime();
 
-var number = doomsDayCount.setTime(doomsDay.getUTCDate());
-number = Date.UTC(2030, 2, 18, 0, 0, 0, 0);
-counter.innerText = number;
+// Upadata the count down every 1 second
+var updateCountDown = setInterval(() => {
+  // Get Today date and time
+  var now = new Date().getTime();
 
-// setInterval(() => {
-//   if (count <= 1000) {
-//     count++;
-//     counter.innerText = count;
-//   }
-// }, 1000);
+  // Find the distance between now and the count down date (doomsDay)
+  var distance = doomsDay - now;
 
-// setTimeout(() => {
-//   followers.innerText = "Followers on Instagram";
-// }, 5000);
+  // Calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the browser
+  counter.innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
+
+  // If the count down is finished, will be written that text below
+  if (distance < 0) {
+    clearInterval(updateCountDown);
+    counter.innerHTML = "The world already ended!";
+  }
+}, 1000);
+
+// Audio
+var audio = <HTMLMediaElement>document.getElementById("audio");
+audio.play();
